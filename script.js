23584c614bec23a1542307186a2f4642f743c17c
shahood zaidi <144329859+Shahoodzaidi@users.noreply.github.com>
2023-10-02 15:51:39 +0530

    Add files via upload
    I had been learning web designing course for the past 5 month now i have finally learnt this... and i have learns 3 languages ( HTML,CSS3,JAVASCRIPT) 
    i have made this  responsive website with the help of these three languages.

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+   
+document.getElementById("icon").style.display="block"
+ document.getElementById("icon1").style.display="none"
+document.getElementById("navlist").style.display="none"
+
+
+
+
+function show(){
+      document.getElementById("icon1").style.display="block"
+      document.getElementById("icon").style.display="none"
+      document.getElementById("navlist").style.display="block"
+ }
+ function hide(){
+      document.getElementById("icon").style.display="block"
+       document.getElementById("icon1").style.display="none"
+     document.getElementById("navlist").style.display="none"
+     
+     
+     
+}
+
+
+
+
+
+ 
